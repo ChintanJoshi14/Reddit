26c942660a701aee869dc277882ebc8c13dba2ab
ChintanJoshi14 <chintan15joshi@gmail.com>
2022-06-03 22:04:34 +0530

    done till @materializer feature adding to getPostList, you forgot to save queries.ts

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -1,5 +1,24 @@
 import { gql } from '@apollo/client'
 
+
+export const GET_ALL_POSTS = gql`
+  query GetAllPosts {
+    getPostList {
+      body
+      created_at
+      id
+      image
+      subreddit_id
+      title
+      username
+    }
+  }
+
+
+
+
+`
+
 export const GET_SUBREDDIT_BY_TOPIC = gql`
   query fetchSubredditByTopic($topic: String!) {
     getSubredditListByTopic(topic: $topic) {
